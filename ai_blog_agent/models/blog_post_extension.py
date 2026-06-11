df645136a6d005dd41cb2670686cf9c54a768ca0
Hamouda-Yasmine <hamouda.yasmine9@gmail.com>
2026-06-11 19:14:33 +0100

    allow multiple article generation per proposal, user decides which to publish

--- ai_blog_agent/models/blog_post_extension.py
+++ ai_blog_agent/models/blog_post_extension.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class BlogPost(models.Model):
     _inherit = 'blog.post'
 
-    ai_proposal_id = fields.Many2one(
+    proposal_id = fields.Many2one(
         'ai.blog.proposal',
         string='AI Proposal',
         readonly=True,
